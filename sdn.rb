4e4c1047d3bc0cc57bb226af06d78178ac65639b
Bilash Saha <bilashsaha03@gmail.com>
2020-09-16 09:05:55 -0600

    Added script to extract and process sdn data from eu and un sources

--- sdn.rb
+++ sdn.rb
@@ -22,7 +22,7 @@ class Sdn
     end
     directory = "../data/downloaded/#{source}/#{time}"
     FileUtils.mkdir_p directory
-    open("#{directory}/sdn.xml", "wb") do |file|
+    open("#{directory}/sanction_list.xml", "wb") do |file|
       file.write(response.body)
     end
   end
@@ -31,19 +31,36 @@ class Sdn
     src_directory = "../data/downloaded/#{source}/#{time}"
     dest_directory = "../data/processed/#{source}/#{time}"
     FileUtils.mkdir_p dest_directory
-    file = open("#{src_directory}/sdn.xml", "r")
+    file = open("#{src_directory}/sanction_list.xml", "r")
     hash = Hash.from_xml(file.read)
     yaml = hash.to_yaml
-    open("#{dest_directory}/sdn.yaml", "w") { |file| file.write(yaml) }
+    open("#{dest_directory}/sanction_list.yaml", "w") { |file| file.write(yaml) }
   end
 
 end
 
+data_sources = [
+  {
+    url: "https://www.treasury.gov/ofac/downloads/sanctions/1.0/sdn_advanced.xml",
+    source: "us"
+  },
+  {
+    url: "https://webgate.ec.europa.eu/fsd/fsf/public/files/xmlFullSanctionsList_1_1/content?token=dG9rZW4tMjAxNw",
+    source: "eu"
+  },
+  {
+    url: "https://scsanctions.un.org/resources/xml/en/consolidated.xml",
+    source: "un"
+  }
+]
 time = Time.now.strftime("%d-%m-%Y-%H:%M:%S")
-url = "https://www.treasury.gov/ofac/downloads/sanctions/1.0/sdn_advanced.xml"
-source = 'us'
-Sdn.download_xml(time, url, source)
-Sdn.convert_to_yaml(time, source)
+data_sources.each do |data_source|
+  url = data_source[:url]
+  source = data_source[:source]
+  Sdn.download_xml(time, url, source)
+  Sdn.convert_to_yaml(time, source)
+  puts "Processed yml file is at:  ../data/processed/#{source}/#{time}/sdn.yaml !"
+end
 
 puts "Done at #{time}!"
-puts "Processed yml file is at:  ../data/processed/#{source}/#{time}/sdn.yaml !"
+
